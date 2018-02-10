8fd19ffd7684fd1948e2d107e864d7fcc3f72549
Tatvam <td14@iitbbs.ac.in>
2018-02-10 22:12:32 +0530

    webpack Dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,9 @@ module :{
         test : /\.js$/,
         exclude : /node_modules/
     }]
+},
+devtool: 'cheap-module-eval-source-map',
+devServer : {
+  contentBase : path.join(__dirname, 'public')
 }
 };
